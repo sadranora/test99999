c3e00679c3903c26c70be052d52d83087ebfd9aa
sadranoras <noras.sadra@gmail.com>
2025-07-21 09:40:03 +0330

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const button = document.querySelector(".button-3");
+
+  button.addEventListener("click", function () {
+    window.location.href = "https://sadranoras.github.io/my-website/";
+    window.open("https://sadranoras.github.io/my-website/", "_blank");
+
+  });
+});
